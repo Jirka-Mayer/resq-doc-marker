5feca427fa66506abb3b66dd140646058c010e18
Jirka-Mayer <jirkamayer97@gmail.com>
2023-08-01 13:16:05 +0200

    Configuring customization name and version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import { bootstrapDocMarker } from "doc-marker"
+import packageJson from "../package.json"
 
 bootstrapDocMarker({
-  element: document.getElementById("doc-marker")
+  element: document.getElementById("doc-marker"),
+  customization: {
+    name: "DocMarker for RES-Q+",
+    version: packageJson.version
+  }
 })
