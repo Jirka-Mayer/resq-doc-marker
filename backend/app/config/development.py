f52464480620b98b7d130d1107c2bdf41cdbc923
Jirka Mayer <jirkamayer97@gmail.com>
2024-05-26 23:30:08 +0200

    Dotenv configuration

--- backend/app/config/development.py
+++ backend/app/config/development.py
@@ -1,10 +1,11 @@
 from .Config import Config
 from pathlib import Path
+import os
 
 development_config = Config(
     resq_api_base_url="https://stroke.dev.qualityregistry.org/api/rest/hpfdc/",
     uploaded_files_storage_path=Path("storage", "development"),
     keycloak_token_url="https://auth.dev.qualityregistry.org/realms/stroke/protocol/openid-connect/token",
     oauth_client_id="doc-marker",
-    oauth_client_password=None,
+    oauth_client_password=os.getenv("DEV_OAUTH_CLIENT_PASSWORD"),
 )
--- backend/app/config/production.py
+++ backend/app/config/production.py
@@ -1,10 +1,11 @@
 from .Config import Config
 from pathlib import Path
+import os
 
 production_config = Config(
     resq_api_base_url="https://stroke.qualityregistry.org/api/rest/hpfdc/",
     uploaded_files_storage_path=Path("storage", "production"),
     keycloak_token_url="https://auth.qualityregistry.org/realms/stroke/protocol/openid-connect/token",
     oauth_client_id="doc-marker",
-    oauth_client_password=None,
+    oauth_client_password=os.getenv("PROD_OAUTH_CLIENT_PASSWORD"),
 )
