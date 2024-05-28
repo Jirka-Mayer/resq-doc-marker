736c13db11ff4e11136233248ac3c87f3b240cae
Jirka Mayer <jirkamayer97@gmail.com>
2024-05-28 10:15:22 +0200

    Renamed oauth client to 'doc-marker' everywhere

--- backend/app/config/development.py
+++ backend/app/config/development.py
@@ -6,6 +6,6 @@ development_config = Config(
     resq_api_base_url="https://stroke.dev.qualityregistry.org/api/rest/hpfdc/",
     uploaded_files_storage_path=Path("storage", "development"),
     keycloak_token_url="https://auth.dev.qualityregistry.org/realms/stroke/protocol/openid-connect/token",
-    oauth_client_id="doc-marker-private",
+    oauth_client_id="doc-marker",
     oauth_client_password=os.getenv("DEV_OAUTH_CLIENT_PASSWORD"),
 )
--- src/config/development.ts
+++ src/config/development.ts
@@ -3,7 +3,7 @@ import { Config } from "./Config";
 const config: Config = {
   resqApiBaseUrl: "https://stroke.dev.qualityregistry.org/api/rest/hpfdc/",
   keycloakAuthUrl: "https://auth.dev.qualityregistry.org/realms/stroke/protocol/openid-connect/auth",
-  oauthClientId: "doc-marker-private",
+  oauthClientId: "doc-marker",
   uploadServerBaseUrl: "https://quest.ms.mff.cuni.cz/resq-doc-marker/backend/",
   uploadServerActInDevelopment: true,
   resqRecordUrl: (id) => `https://stroke.dev.qualityregistry.org/data-collection/record/${id}/form`,
