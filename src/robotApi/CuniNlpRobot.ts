0dd9c2eb7bfbdd9d088fc563755b4c1052360c95
Jirka Mayer <jirkamayer97@gmail.com>
2026-06-05 17:33:24 +0200

    Graceful robot aborting and 429 handling

--- src/robotApi/CuniNlpRobot.ts
+++ src/robotApi/CuniNlpRobot.ts
@@ -27,37 +27,46 @@ const LIMIT_EVIDENCE_ANSWERS_TO = 5;
 const AP_MINIMAL_QUESTION_CONFIDENCE = 0.8;
 const AP_MINIMAL_QUESTION_FREQUENCY = 0.1;
 
+// TODO: on abort signal return null, not throw error
+
 // Swagger endpoint is at https://quest.ms.mff.cuni.cz/dimbu/docs
 
+/**
+ * An awaitable variant of the setTimeout function.
+ * Usage: await timeoutAsync(500);
+ */
+function timeoutAsync(delay?: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, delay));
+}
+
 export class CuniNlpRobot implements RobotInterface {
+  /**
+   * Process at most 10 fields at a time (in parallel)
+   * Which is the batch size sent to the Dimbu server
+   */
+  public readonly maxFieldRequestConcurrency: number = 5;
+
   /**
    * Extracts a list of evidences for a single form field (a single question)
    */
   public async extractEvidences(
     request: EvidenceExtractionRequest,
     abortSignal: AbortSignal,
-  ): Promise<EvidenceExtractionResponse> {
-    const dimbuRequest: DimbuEvidenceExtractionRequest = {
-      context: request.reportText,
-      question_ids: [request.fieldId], // we always send exactly 1 question
-      language: request.reportLanguage, // ISO 639 Language Code
-      form_version: null, // Dimbu currently does not use this
-    };
-    const httpResponse = await fetch(DIMBU_URL + "/api/wp4/extract_evidences", {
-      headers: {
-        "Content-Type": "application/json",
+  ): Promise<EvidenceExtractionResponse | null> {
+    const dimbuResponse = await this.fetchEvidenceExtraction(
+      {
+        context: request.reportText,
+        question_ids: [request.fieldId], // we always send exactly 1 question
+        language: request.reportLanguage, // ISO 639 Language Code
+        form_version: null, // Dimbu currently does not use this
       },
-      method: "POST",
-      body: JSON.stringify(dimbuRequest),
-      signal: abortSignal,
-    });
+      abortSignal,
+    );
 
-    if (!httpResponse.ok) {
-      throw Error("Dimbu request resulted in a non 200-ok response.");
+    // graceful aborting
+    if (dimbuResponse === null) {
+      return null;
     }
-
-    const dimbuResponse: DimbuEvidenceExtractionResponse =
-      await httpResponse.json();
 
     if (dimbuResponse.predictions.length !== 1) {
       throw Error(
@@ -126,38 +135,79 @@ export class CuniNlpRobot implements RobotInterface {
   }
 
   /**
+   * Sends the HTTP fetch request, returns null on abort and throws on failure
+   */
+  private async fetchEvidenceExtraction(
+    request: DimbuEvidenceExtractionRequest,
+    abortSignal: AbortSignal,
+  ): Promise<DimbuEvidenceExtractionResponse | null> {
+    try {
+      for (let tries = 0; tries <= 5; tries++) {
+        // wait 0s, 2s, 4s, 8s, 16s, 32s before re-try (for 429 responses)
+        if (tries > 0) {
+          await timeoutAsync((2 << (tries - 1)) * 1_000);
+        }
+
+        const httpResponse = await fetch(
+          DIMBU_URL + "/api/wp4/extract_evidences",
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+            method: "POST",
+            body: JSON.stringify(request),
+            signal: abortSignal,
+          },
+        );
+
+        if (!httpResponse.ok) {
+          throw Error("Dimbu request resulted in a non 200-ok response.");
+        }
+
+        if (httpResponse.status === 429) {
+          continue; // re-try
+        }
+
+        return (await httpResponse.json()) as DimbuEvidenceExtractionResponse;
+      }
+    } catch (e) {
+      // abortion should be graceful
+      if (abortSignal.aborted) {
+        return null;
+      }
+      throw e;
+    }
+
+    // this calms down return-value typescript checks
+    throw new Error("This line should not be executed.");
+  }
+
+  /**
    * Attempts to predict an anwer for a form field (a question) given
    * the set of previously-extracted (or human-provided) evidences
    */
   public async predictAnswer(
     request: AnswerPredictionRequest,
     abortSignal: AbortSignal,
-  ): Promise<AnswerPredictionResponse> {
-    const dimbuRequest: DimbuAnswerPredictionRequest = {
-      questions: [
-        {
-          question_id: request.fieldId,
-          evidences: request.evidences.map((e) => e.text),
-        },
-      ],
-      language: request.reportLanguage, // ISO 639 Language Code
-      form_version: null, // Dimbu falls back to the latest form it know about
-    };
-    const httpResponse = await fetch(DIMBU_URL + "/api/wp4/predict_answers", {
-      headers: {
-        "Content-Type": "application/json",
+  ): Promise<AnswerPredictionResponse | null> {
+    const dimbuResponse = await this.fetchAnswerPrediction(
+      {
+        questions: [
+          {
+            question_id: request.fieldId,
+            evidences: request.evidences.map((e) => e.text),
+          },
+        ],
+        language: request.reportLanguage, // ISO 639 Language Code
+        form_version: null, // Dimbu falls back to the latest form it know about
       },
-      method: "POST",
-      body: JSON.stringify(dimbuRequest),
-      signal: abortSignal,
-    });
+      abortSignal,
+    );
 
-    if (!httpResponse.ok) {
-      throw Error("Dimbu request resulted in a non 200-ok response.");
+    // graceful aborting
+    if (dimbuResponse === null) {
+      return null;
     }
-
-    const dimbuResponse: DimbuAnswerPredictionResponse =
-      await httpResponse.json();
 
     if (dimbuResponse.predictions.length !== 1) {
       throw Error("We expect 1 answer prediction to be returned by the model.");
@@ -200,5 +250,53 @@ export class CuniNlpRobot implements RobotInterface {
       metadata: metadata,
       modelVersion: dimbuResponse.model_version,
     };
+  }
+
+  /**
+   * Sends the HTTP fetch request, returns null on abort and throws on failure
+   */
+  private async fetchAnswerPrediction(
+    request: DimbuAnswerPredictionRequest,
+    abortSignal: AbortSignal,
+  ): Promise<DimbuAnswerPredictionResponse | null> {
+    try {
+      for (let tries = 0; tries <= 5; tries++) {
+        // wait 0s, 2s, 4s, 8s, 16s, 32s before re-try (for 429 responses)
+        if (tries > 0) {
+          await timeoutAsync((2 << (tries - 1)) * 1_000);
+        }
+
+        const httpResponse = await fetch(
+          DIMBU_URL + "/api/wp4/predict_answers",
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+            method: "POST",
+            body: JSON.stringify(request),
+            signal: abortSignal,
+          },
+        );
+
+        if (!httpResponse.ok) {
+          throw Error("Dimbu request resulted in a non 200-ok response.");
+        }
+
+        if (httpResponse.status === 429) {
+          continue; // re-try
+        }
+
+        return (await httpResponse.json()) as DimbuAnswerPredictionResponse;
+      }
+    } catch (e) {
+      // abortion should be graceful
+      if (abortSignal.aborted) {
+        return null;
+      }
+      throw e;
+    }
+
+    // this calms down return-value typescript checks
+    throw new Error("This line should not be executed.");
   }
 }
